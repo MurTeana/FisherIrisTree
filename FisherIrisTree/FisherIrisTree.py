b4e9ce6f5909d2ecb455aa8024b3acd4450d1739
MurTeana <sm.teana@gmail.com>
2020-11-14 21:30:34 +0300

    var3lab6

--- FisherIrisTree/FisherIrisTree.py
+++ FisherIrisTree/FisherIrisTree.py
@@ -1,26 +1,32 @@
 import numpy as np
-import numpy as npy
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-x=np.array([
-    [1,1,1,1],
-    [1,1,1,2],
-    [2,1,1,1],
-    [3,2,1,1],
-    [3,3,2,1],
-    [3,3,2,2],
-    [2,3,2,2],
-    [1,2,1,1],
-    [1,3,2,1],
-    [3,2,2,1],
-    [1,2,2,2],
-    [2,2,1,2],     
-    [2,1,2,1], 
-    [3,2,1,2]
-    ])
-y=npy.array([1,1,2,2,2,1,2,1,2,2,2,2,2,1])
-from sklearn.tree import DecisionTreeClassifier
-tree = DecisionTreeClassifier(random_state=0)
-tree.fit(x,y)
-print('Правильность на обучающем наборе:{:.3f}'.format(tree.score(x,y)))
-print('Важность признаков: \n{}'.format(tree.feature_importances_))
+datafile=pd.read_csv("C:/Users/St.Eva/Desktop/Projects Python/FisherIrisTree/IrisFisher.csv")
+print (datafile.head())
 
+x=datafile.iloc[:,:1]
+y=datafile.iloc[:,1:]
+
+#print (x.head())
+#print (y.head())
+
+#построим модель с помощью sklearn
+from sklearn import tree
+model=tree.DecisionTreeClassifier(criterion="entropy")
+print(model.fit(x,y))
+#оценка модели
+print(model.score(x,y))
+print('Правильность на обучающем наборе:{:.3f}'.format(model.score(x,y)))
+print('Важность признаков: \n{}'.format(model.feature_importances_))
+#прогнозирование с помощью модели
+print('С данной длиной ирис будет отнесен к классу ')
+print(model.predict([[12]]))
+print('С данной длиной ирис будет отнесен к классу ')
+print(model.predict([[29]]))
+print('С данной длиной ирис будет отнесен к классу ')
+print(model.predict([[49]]))
+#
+#визуализация модели
+#print(tree.plot_tree(model))
