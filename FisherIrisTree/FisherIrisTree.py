bd7136286eec3f554a39e434789841ebc9f05ab3
AlexandraVarfolomeyeva <AAV998@yandex.ru>
2020-11-14 17:33:35 +0300

    обратно

--- FisherIrisTree/FisherIrisTree.py
+++ FisherIrisTree/FisherIrisTree.py
@@ -21,5 +21,5 @@ y=npy.array([1,1,2,2,2,1,2,1,2,2,2,2,2,1])
 from sklearn.tree import DesisionTreeClassifier
 tree = DesisionTreeClassifier(random_state=0)
 tree.fit(x,y)
-print('--Правильность на обучающем наборе:{:.3f}'.format(tree.score(x,y)))
+print('Правильность на обучающем наборе:{:.3f}'.format(tree.score(x,y)))
 print('Важность признаков: \n{}'.format(tree.feauture_importance_))
